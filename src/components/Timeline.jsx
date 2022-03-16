1d0b1984c121ae847de46ef57421880d0b59db4d
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-03-17 02:56:45 +0800

    CI npm run build

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,13 +4,27 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TimelineMiddleComponents from "./TimelineMiniComponents/TimelineMiddleComponent";
 import RightTimelineMainComponent from "./TimelineMiniComponents/RightTimelineMainComponent";
-
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 const Timeline = () => {
 	return (
 		<div className='w-full flex bg-slate-900 h-auto pb-5 '>
 			<RightTimelineMainComponent />
 			<TimelineMiddleComponents />
-			<div className='sticky md:w-2/5 lg:w-3/12 h-screen hidden md:block'></div>
+			<section className='sticky top-0 left-0 w-3/12 h-screen overflow-y-scroll scrollbar scrollbar-thin scrollbar-thumb-slate-600 scrollbar-track-gray-400 hidden lg:block '>
+				<div className='flex flex-col items-start justify-center px-4 mt-4 space-y-2 text-gray-300 font-medium divide-y divide-gray-400 '>
+					<div className='space-y-2 mt-4'>
+						<section className='flex items-center space-x-2'>
+							<div className='flex w-12 h-12 justify-start items-center'>
+								<FontAwesomeIcon
+									icon={faBookmark}
+									className='h-10 w-10 text-purple-700'
+								/>
+							</div>
+							<p>Saved</p>
+						</section>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 };
